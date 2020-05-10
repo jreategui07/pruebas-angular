fc989dd413037b37a01ff41d44c7e381af4b0d5c
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-10 13:51:04 -0500

    Pruebas de booleanos

---
+++ src/app/basicas/numeros/booleanos/booleanos.spec.ts
@@ -0,0 +1,10 @@
+import { usuarioIngresado } from './booleanos';
+
+describe('Pruebas de booleanos', () => {
+
+  it('Debe retornar true', () => {
+    const resp = usuarioIngresado();
+    expect(resp).toBeTrue();
+  });
+
+});
