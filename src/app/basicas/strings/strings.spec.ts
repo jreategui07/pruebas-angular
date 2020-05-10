d2f307e760768f32487fd7901d5e5f996b887dcb
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-10 13:22:16 -0500

    Comprobar que el resultado contenga algo

--- src/app/basicas/strings/strings.spec.ts
+++ src/app/basicas/strings/strings.spec.ts
@@ -3,8 +3,14 @@ import { saludar } from './strings';
 describe('Prueba de cadenas', () => {
 
   it('Debe regresar una cadena', () => {
-    const mensaje = saludar('Jonathan');
-    expect(typeof mensaje).toBe('string');
+    const resp = saludar('Jonathan');
+    expect(typeof resp).toBe('string');
+  });
+
+  it('Debe contener el nombre enviado', () => {
+    const nombre = 'Jonathan';
+    const resp = saludar(nombre);
+    expect(resp).toContain(nombre);
   });
 
 });
