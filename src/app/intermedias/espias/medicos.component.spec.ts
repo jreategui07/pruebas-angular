110564394148a3e1c3d93474daf6659007cb8f48
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-10 21:17:08 -0500

    Actualizaciones del meterial de apoyo

---
+++ src/app/intermedias/espias/medicos.component.spec.ts
@@ -0,0 +1,18 @@
+import { MedicosComponent } from './medicos.component';
+import { MedicosService } from './medicos.service';
+
+describe('MedicosComponent', () => {
+
+  let componente: MedicosComponent;
+
+  beforeEach(() => {
+    
+  });
+
+
+  it('', () => {
+
+  });
+
+
+});
