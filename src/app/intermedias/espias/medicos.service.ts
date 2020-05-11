e7059d4f360eaba57048fbb21197e066e6c75f3b
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-10 21:25:08 -0500

    Actualizaciones

--- src/app/intermedias/espias/medicos.service.ts
+++ src/app/intermedias/espias/medicos.service.ts
@@ -9,18 +9,21 @@ export class MedicosService {
 
   getMedicos() {
     return this.http.get('...').pipe(
+      // tslint:disable-next-line: no-string-literal
       map(resp => resp['medicos'])
     );
   }
 
   agregarMedico(medico: any) {
     return this.http.post('...', medico).pipe(
+      // tslint:disable-next-line: no-string-literal
       map(resp => resp['medico'])
     );
   }
 
   borrarMedico(id: string) {
     return this.http.delete('...').pipe(
+      // tslint:disable-next-line: no-string-literal
       map(resp => resp['medico'])
     );
   }
