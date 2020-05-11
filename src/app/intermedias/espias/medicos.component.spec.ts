0e07c3912865f09d563ba9a1b00b5b3ebfcf957c
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-10 21:55:29 -0500

    Pruebas de Espias

--- src/app/intermedias/espias/medicos.component.spec.ts
+++ src/app/intermedias/espias/medicos.component.spec.ts
@@ -1,17 +1,22 @@
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
+import { from } from 'rxjs';
 
 describe('MedicosComponent', () => {
 
   let componente: MedicosComponent;
+  const servicio = new MedicosService(null);
 
   beforeEach(() => {
-    
+    componente = new MedicosComponent(servicio);
   });
 
-
-  it('', () => {
-
+  it('Init: Se deben cargar los médicos', () => {
+    spyOn(servicio, 'getMedicos').and.callFake(() => {
+      return from([ 'medico1', 'medico2', 'medico3' ]);
+    });
+    componente.ngOnInit();
+    expect(componente.medicos.length).toBeGreaterThan(0);
   });
 
 
