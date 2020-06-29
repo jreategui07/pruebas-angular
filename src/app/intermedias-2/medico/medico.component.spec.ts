41e0dd9a8bffc6315937d4119560660c057ff04e
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-06-29 16:29:44 -0500

    Documentación de configuraciones de pruebas de integración

--- src/app/intermedias-2/medico/medico.component.spec.ts
+++ src/app/intermedias-2/medico/medico.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { MedicoComponent } from './medico.component';
 
+// TestBed: Es una clase que contiene un conjunto de métodos y funciones que nos van a permitir realizar pruebas de elementos y componentes de Angular
+
 describe('Pruebas del component: MedicoComponent', () => {
 
   let component: MedicoComponent;
@@ -8,12 +10,12 @@ describe('Pruebas del component: MedicoComponent', () => {
 
   beforeEach(() => {
 
-    TestBed.configureTestingModule({
+    TestBed.configureTestingModule({ // configureTestingModule: Nos permite definir un módulo donde declararemos los elementos necesarios para hacer trabajar nuestro componente
       declarations: [MedicoComponent]
     });
 
-    fixture = TestBed.createComponent(MedicoComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(MedicoComponent); // createComponent: Nos permite crear un compoente ya proocesado y compilado por el testbed. TestBed.createComponent, nos devuelve un fixture eu cual nos va a permitir tener acceso al html, componentes del dom, entre otros
+    component = fixture.componentInstance; // fixture.componentInstance: Nos permite tener acceso a la instancia del componente
 
   });
 
