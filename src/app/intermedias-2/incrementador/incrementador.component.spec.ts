2b3ecad4e7468839fe0a488ac394985362181b9c
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-06-29 22:23:32 -0500

    Realizando pruebas en el html del un componente

--- src/app/intermedias-2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedias-2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -19,9 +20,16 @@ describe('Incremendator Component', () => {
 
     });
 
-    it('', () => {
+    it('Se debe crear el componenten IncrementadorComponent', () => {
+        expect(component).toBeTruthy();
+    });
 
-
+    it('Se debe mostrar la leyenda', () => {
+        const leyendaPrueba = 'Progreso de carga';
+        component.leyenda = leyendaPrueba;
+        fixture.detectChanges();
+        const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+        expect(elem.innerHTML).toContain(leyendaPrueba);
     });
 
 });
